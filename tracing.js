4fe4ad7479053cb20e26442aea69134ca8c64385
yixuanyang123 <yixuanyang@ufl.edu>
2024-04-23 01:35:31 -0400

    Update tracing.js to include Jaeger exporter

--- tracing.js
+++ tracing.js
@@ -1,29 +1,34 @@
 const { Resource } = require("@opentelemetry/resources");
-const { SemanticResourceAttributes } = 
-require("@opentelemetry/semantic-conventions");
-const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-base');
-const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
+const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
+const { ConsoleSpanExporter, SimpleSpanProcessor } = require('@opentelemetry/sdk-trace-base');
 const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
 const { trace } = require("@opentelemetry/api");
-//Instrumentations
-const { ExpressInstrumentation } = 
-require("opentelemetry-instrumentation-express");
-const { MongoDBInstrumentation } = 
-require("@opentelemetry/instrumentation-mongodb");
-const { HttpInstrumentation } = 
-require("@opentelemetry/instrumentation-http");
-const { registerInstrumentations } = 
-require("@opentelemetry/instrumentation");
-//Exporter
+// Instrumentations
+const { ExpressInstrumentation } = require("opentelemetry-instrumentation-express");
+const { MongoDBInstrumentation } = require("@opentelemetry/instrumentation-mongodb");
+const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
+const { registerInstrumentations } = require("@opentelemetry/instrumentation");
+// Jaeger Exporter
+const { JaegerExporter } = require('@opentelemetry/exporter-jaeger');
+
 module.exports = (serviceName) => {
-   const exporter = new ConsoleSpanExporter();
+   const consoleExporter = new ConsoleSpanExporter();
+   const jaegerExporter = new JaegerExporter({
+       endpoint: 'http://localhost:14268/api/traces'
+   });
+
    const provider = new NodeTracerProvider({
        resource: new Resource({
            [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
        }),
    });
-   provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
+
+   // You can add multiple span processors if you want to export to multiple places
+   provider.addSpanProcessor(new SimpleSpanProcessor(consoleExporter));
+   provider.addSpanProcessor(new SimpleSpanProcessor(jaegerExporter));
+
    provider.register();
+
    registerInstrumentations({
        instrumentations: [
            new HttpInstrumentation(),
@@ -32,5 +37,7 @@ module.exports = (serviceName) => {
        ],
        tracerProvider: provider,
    });
+
    return trace.getTracer(serviceName);
 };
+
